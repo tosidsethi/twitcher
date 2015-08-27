6f6a4a676004cd4f4cfcccf3cb2941ce6ca1a746
SidSTi <siddhant.sethi91@gmail.com>
2015-08-27 12:18:19 -0500

    code cleanup

--- scripts/twitcher.js
+++ scripts/twitcher.js
@@ -121,21 +121,12 @@
 			};
 		}});
 
-		this.list = o.list || {};
+		this.bufferList = o.bufferList || {};
 
 		Twitcher.all.push(this);
  	} 
 
  	Twitcher.prototype = {
-		set list(list) {
-			if (typeof list === 'object') {
-				this.bufferList = list;
-			}
-			
-			if (document.activeElement === this.input) {
-				this.evaluateList();
-			}
-		},
 
 		get selected() {
 			return this.index > -1;
@@ -416,39 +407,38 @@
 	return Twitcher;
  })();
 
- var $jsonp = (function(){
-  var result = {};
+var $jsonp = (function(){
+	var result = {};
 
-  result.send = function(src, options) {
-    var options = options || {},
-      callback_name = options.callbackName || 'callback',
-      on_success = options.onSuccess || function(){},
-      on_timeout = options.onTimeout || function(){},
-      timeout = options.timeout || 10;
+  	result.send = function(src, options) {
+    	var options = options || {},
+      	callback_name = options.callbackName || 'callback',
+      	on_success = options.onSuccess || function(){},
+      	on_timeout = options.onTimeout || function(){},
+      	timeout = options.timeout || 10;
 
-    var timeout_trigger = window.setTimeout(function(){
-      window[callback_name] = function(){};
-      on_timeout();
-    }, timeout * 1000);
+    	var timeout_trigger = window.setTimeout(function(){
+      		window[callback_name] = function(){};
+      		on_timeout();
+    		}, timeout * 1000);
 
-    window[callback_name] = function(data){
-      window.clearTimeout(timeout_trigger);
-      on_success(data);
-    };
+    	window[callback_name] = function(data){
+      		window.clearTimeout(timeout_trigger);
+      		on_success(data);
+    	};
 
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.async = true;
-    script.src = src;
+    	var script = document.createElement('script');
+    	script.type = 'text/javascript';
+    	script.async = true;
+    	script.src = src;
 
-    document.getElementsByTagName('head')[0].appendChild(script);
+    	document.getElementsByTagName('head')[0].appendChild(script);
 
-    script.onload = function () {
+    	script.onload = function () {
             this.remove();
-        };
-  };
-
-  return result;
+        }
+  	}
+  	return result;
 })();
 
  
